406e89786ffdb19264359420247d66d5ef291f1c
sandeepsharma-kgp <sandeepsharma.iit@gmail.com>
2019-07-17 01:37:06 +0530

    check7

--- dist/js/about.72322d0e.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{a1d1:function(t,e,a){"use strict";a.r(e);var n=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},r=[function(){var t=this,e=t.$createElement,a=t._self._c||e;return a("div",{staticClass:"about",staticStyle:{position:"realtive",top:"100px"}},[a("br"),a("br"),a("br"),a("br"),a("h1",[t._v("This is an about page")])])}],s=a("2877"),i={},l=Object(s["a"])(i,n,r,!1,null,null,null);e["default"]=l.exports}}]);
-//# sourceMappingURL=about.72322d0e.js.map
